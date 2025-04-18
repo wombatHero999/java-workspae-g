f43f9065d07200fe7fb59f1321155173e7c250fa
wombathero999 <wombathero999@gmail.com>
2025-04-18 15:01:32 +0900

    init

---
+++ 01_firstproject/src/com/kh/run/FirstMyObject.java
@@ -0,0 +1,26 @@
+package com.kh.run;
+//package + class명 == 풀 클래스명
+
+import java.util.Date; // 날짜를 표시해주는 클래스
+
+public class FirstMyObject {
+	// 자바는 실행하기 위해서 반드시 main 메서드가 존재해야 한다.
+	public static void main(String[] args) {
+		System.out.println("안녕1");
+		
+		// 외부 클래스를 이용하여 현재 날짜를 출력
+		System.out.println(  new Date()  );
+	}
+}
+
+// ex_firstproject라는 프로젝트를 생성한 후 
+// com.ex.run패키지에 Test클래스를 만들고, 자신의 이름을 console창에
+// 출력해보세요.
+
+
+
+
+
+
+
+
